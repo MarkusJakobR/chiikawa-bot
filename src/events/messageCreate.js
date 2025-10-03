ecea7df15df5bb58cf815d4aaf9d2df6f607ebb0
Markus Recaplaza <markusrecaplaza@Markuss-MacBook-Air.local>
2025-10-03 09:57:05 +0800

    fix: checks messages only when there is an active game

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,9 +1,12 @@
-import { handleGuess } from "../services/hangman/hangmanService.js";
+import { handleGuess, getGame } from "../services/hangman/hangmanService.js";
 
 // for creating a message using handleGuess
 export default {
   name: "messageCreate",
   async execute(message) {
-    await handleGuess(message);
+    const game = getGame(message.channel.id);
+    if (game) {
+      await handleGuess(message);
+    }
   },
 };
